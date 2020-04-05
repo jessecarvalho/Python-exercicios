71028096486417929d77f0d0b58db8140420cf5d
jesseCarvalho <jesseelias80@gmail.com>
2020-04-05 10:57:33 -0300

    new exercises added

---
+++ aprovandoEmprestimo.py
@@ -0,0 +1,16 @@
+#Escreva um programa para aprovar o empréstimo bancário para a compra de uma casa.
+#Pergunte o valor da casa, o salario do comprador e em quantos anos vai pagar.
+#A prestação mensal não pode exceder 30% do salário ou então o empréstimo será negado.
+#Exercicio pensado por: Gustavo Guanabará / Curso em vídeo
+
+valorCasa = float(input("Digite o valor da casa: "))
+salario = float(input("Digite seu salário: "))
+anos = int(input("Em quantos anos irá pagar? "))
+meses = anos * 12
+mensalidade = valorCasa / meses
+limiteMaximo = (salario / 100) * 30
+if limiteMaximo < mensalidade:
+    print("Empréstimo não aprovado!")
+else:
+    print("Empréstimo aprovado com sucesso!")
+    print(f"A mensalidade ficará no custo de R${mensalidade}")
