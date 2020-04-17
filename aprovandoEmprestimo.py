a46e02867f8d688ab1b101e1d568150553ce736c
jesseCarvalho <jesseelias80@gmail.com>
2020-04-17 19:07:12 -0300

    Correcting files

--- aprovandoEmprestimo.py
+++ aprovandoEmprestimo.py
@@ -4,14 +4,18 @@
 # A prestação mensal não pode exceder 30% do salário ou então o empréstimo será negado.
 # Exercicio pensado por: Gustavo Guanabará / Curso Em Vídeo
 
-valorCasa = float(input("Digite o valor da casa: "))
-salario = float(input("Digite seu salário: "))
-anos = int(input("Em quantos anos irá pagar? "))
-meses = anos * 12
-mensalidade = valorCasa / meses
-limiteMaximo = (salario / 100) * 30
-if limiteMaximo < mensalidade:
-    print("Empréstimo não aprovado!")
-else:
-    print("Empréstimo aprovado com sucesso!")
-    print(f"A mensalidade ficará no custo de R${mensalidade}")
+def emprestimo():
+    valorCasa = float(input("Digite o valor da casa: "))
+    salario = float(input("Digite seu salário: "))
+    anos = int(input("Em quantos anos irá pagar? "))
+    meses = anos * 12
+    mensalidade = valorCasa / meses
+    limiteMaximo = (salario / 100) * 30
+    if limiteMaximo < mensalidade:
+        print("Empréstimo não aprovado!")
+    else:
+        print("Empréstimo aprovado com sucesso!")
+        print(f"A mensalidade ficará no custo de R${mensalidade}")
+
+if __name__ == '__main__':
+    emprestimo()
--- calculandoDescontos.py
+++ calculandoDescontos.py
@@ -5,8 +5,11 @@
 # Exercicio pensado por: Gustavo Guanabará / Curso Em Vídeo
 
 
+def calculaDec(valorProduto, desconto):
+    valorProdutoComDesc = valorProduto - ((valorProduto / 100) * desconto)
+    print(f"O produto com desconto de {desconto}% fica: R${valorProdutoComDesc}")
 
-valorProduto = int(input("Digite o valor do produto a ser comprado: "))
-desconto = int(input("Digite o desconto a ser aplicado: "))
-valorProdutoComDesc = valorProduto - ((valorProduto / 100) * desconto)
-print(f"O produto com desconto de {desconto}% fica: R${valorProdutoComDesc}")
+if __name__ == '__main__':
+    valorProduto = int(input("Digite o valor do produto a ser comprado: "))
+    desconto = int(input("Digite o desconto a ser aplicado: "))
+    calculaDec(valorProduto, desconto)
